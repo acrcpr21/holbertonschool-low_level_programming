e47faf1611e29b20ddb593dfe3308fd22bda07a0
alexander <acrcpr@gmail.com>
2025-11-02 23:39:22 -0400

    updating repo

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+ int _strlen(char *s)
+ {
+     int length = 0;
+
+     while (s[length] != '\0')
+     {
+         length++;
+     }
+     return (length);
+}
