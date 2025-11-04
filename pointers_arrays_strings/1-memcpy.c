45c30174afcc14c2f648d480813a3a1939a2f1f2
alexander <acrcpr@gmail.com>
2025-11-04 14:57:36 -0400

    Fix _memcpy: Betty style commetns and return statement

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * _memcpy "Will copy the memory area."
- * @dest "It is the memory area to copy towards from the destination."
- * @src "It is the memory area to copy from the source."
- * @n "It is the number of the bytes to copy."
- *
- * return "It is the pointer to the destination memory area destination."
- */
-
+* _memcpy -copies memory area
+* @dest: -It is the memory area to copy towards from the destination.
+* @src: -It is the memory area to copy from the source.
+* @n: -It is the number of the bytes to copy.
+*
+* Return: -It is the pointer to the destination memory area destination.
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
@@ -18,6 +17,5 @@ for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
 }
-
-return (dest);
+Return(dest);
 }
