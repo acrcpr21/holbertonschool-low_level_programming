b4e9c6d4a2725188d3c0c4860e618043b6c05f46
alexander <acrcpr@gmail.com>
2025-11-15 14:20:03 -0400

    fixed betty issues

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,19 +2,19 @@
 #include <stddef.h>
 
 /**
- * array_iterator - executes a function on each element of an array
- * @array: pointer to the integer array
- * @size: size of the array
- * @action: pointer to the function to execute on each array element
- */
+* array_iterator - executes a function on each element of an array
+* @array: pointer to the integer array
+* @size: size of the array
+* @action: pointer to the function to execute on each array element
+*/
 
 /**
- * array_interator - a Function that ...
- * @array: Description of array.
- * @size: Description of size.
- * @(*action: Description of (*action.
- * Return: Description of the return value.
- */
+* array_interator - a Function that ...
+* @array: Description of array.
+* @size: Description of size.
+* @(*action: Description of (*action.
+* Return: Description of the return value.
+*/
 void array_interator(int *array, size_t size, void (*action)(int))
 {
     size_t i;
