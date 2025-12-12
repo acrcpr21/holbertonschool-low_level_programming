d7c2c13ca728c95dba1cff70c102eb2dbf9743de
alexander <acrcpr@gmail.com>
2025-12-12 06:19:10 -0400

    Uploaded Task files 0-9 for C - Variables, if, else, while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,16 @@ int main(void)
      srand(time(0));
      n = rand() - RAND_MAX / 2;
      if (n > 0)
-         printf("%d is positive\n", n);.*%d= integer; ,n outside the ) = the int n*/
+     {
+         printf("%d is positive\n", n); /*%d= integer; ,n outside the ) = the int n*/
+     }
          else if (n == 0)
+         {
              printf("%d is zero\n", n);
+         }
              else
+             {
                  printf("%d is negative\n", n);
+             }
                  return (0);
 }
