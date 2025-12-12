dd225a79fd0873dc1c364a6af3b16fa7030307d9
alexander <acrcpr@gmail.com>
2025-12-12 07:19:46 -0400

    Uploaded Task files 0-9 for C - Variables, if, else, while

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,11 @@ int main(void)
 /* variable characters/letters*/
 char letter;
 /*Loop with ASCII values of the lowercase letters (a-z)*/
-for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter); /*this will print each letter, one by one*/
-putchar('\n'); /*this prints a new line after the alphabet code*/
+for
+(letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+/*this will print each letter, one by one*/
+putchar('\n');
+/*this prints a new line after the alphabet code*/
 return(0);
 }
