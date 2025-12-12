3eb2503b6e079f674c94444d27c9552c36b37d0e
alexander <acrcpr@gmail.com>
2025-12-12 08:13:41 -0400

    Uploaded Task files 0-9 for C - Variables, if, else, while

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,11 +7,14 @@ int main(void)
 {
 int numbers;
 for (numbers = 48; numbers <= 57; numbers++)
+{
 putchar(numbers);
 if (numbers < 57)
-
+{
 putchar(',');
 putchar(' ');
-
+}
+}
+putchar('\n');
 return (0);
 }
