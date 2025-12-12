123497653213f3ef4aac068cc1dd46783ecb2d53
alexander <acrcpr@gmail.com>
2025-12-12 06:03:35 -0400

    Uploaded Task files 0-9 for C - Variables, if, else, while

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry main do while numbers
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+    int number;
+    number = 48;
+    do {
+        putchar(number);
+        number++;
+
+    } while (number <= 57);
+
+    putchar ('\n');
+    return (0)
+}
