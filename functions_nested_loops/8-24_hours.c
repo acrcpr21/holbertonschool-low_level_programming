079ad26d611ef813c20e1183581850a1defb3f77
alexander <acrcpr@gmail.com>
2025-12-14 19:56:25 -0400

    Uploaded Task files 8 for C-Functions, nested loops

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,27 +1,24 @@
 #include "main.h"
-#include <time.h>
 
 /**
- * print_time - prints the current time in HH:MM format (24-hour clock)
- *
- * Return: void
- */
-
+* jack_bauer - prints every minute of the day, 24-hour clock
+*
+* Return: void
+*/
 void jack_bauer(void)
 {
-    time_t t = time(NULL);
-    struct tm *tm_info = localtime(&t);
-    int hour = tm_info->tm_hour;
-    int minute = tm_info->tm_min;
-    int h1 = hour / 10;
-    int h2 = hour % 10;
-    int m1 = minute / 10;
-    int m2 = minute % 10;
+int h, m;
 
-    _putchar(h1 + '0');
-    _putchar(h2 + '0');
-    _putchar(':');
-    _putchar(m1 + '0');
-    _putchar(m2 + '0');
-    _putchar('\n');
+for (h = 0; h <= 23; h++)
+{
+for (m = 0; m <= 59; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
 }
