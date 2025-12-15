bc601c145136f482721f3e20e18ad75444b8367a
alexander <acrcpr@gmail.com>
2025-12-15 00:28:10 -0400

    Updated file 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,7 +8,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
-		*dest++ = *src++;
-	return (dest);
+int i = 0;
+while (src[i] != '\0')
+dest[i] = src[i];
+i++;
+dest[i] = '\0';
+Ureturn (dest);
 }
