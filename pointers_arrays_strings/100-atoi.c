4c0e552832dbbe5476d5f4feb6b58ca5071ba73e
alexander <acrcpr@gmail.com>
2025-12-15 00:17:35 -0400

    Updated file 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,25 @@
  */
 int _atoi(char *s)
 {
-	while (*s)
-		_putchar(*s++);
-	return (0);
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] == ' ')
+		i++;
+
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
 }
