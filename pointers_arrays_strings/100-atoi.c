26f22ca864138a5008932867e8278a8f627d3a1f
alexander <acrcpr@gmail.com>
2025-12-15 00:22:48 -0400

    Uploaded file 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,23 +7,26 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
 	int i = 0;
+	int sign = 1;
+	unsigned int result = 0;
+	int digit_found = 0;
 
-	while (s[i] == ' ')
-		i++;
-
-	if (s[i] == '-' || s[i] == '+')
+	while (s[i] != '\0')
 	{
-		if (s[i] == '-')
-			sign = -1;
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit_found = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (digit_found)
+		{
+			break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
 		i++;
 	}
 
