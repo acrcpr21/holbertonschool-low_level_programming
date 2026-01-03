5a5caf37c70a50d86f3ba5b3f7c99348f8cdc782
acrcpr21 <acrcpr@gmail.com>
2026-01-02 21:46:09 -0400

    Correcting Betty Errors

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,7 +6,6 @@
  * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the list
  * @str: string to add
- * @len: length of the string
  * Return: the address of the new element, or NULL if it failed
  */
 
