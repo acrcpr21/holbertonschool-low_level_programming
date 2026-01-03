c4469b5cafdc2eb9896302f6f19641d083a7e6fd
acrcpr21 <acrcpr@gmail.com>
2026-01-02 21:44:49 -0400

    Correcting Betty Errors

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@
  * Return: the address of the new element, or NULL if it failed
  */
 
- list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *new_str;
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,7 +37,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new_node;
 		return (new_node);
 	}
-	
+
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
