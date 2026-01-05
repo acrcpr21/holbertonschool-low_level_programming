2ec4793c044c2a1edf95830c33e4acbaac5425d7
acrcpr21 <acrcpr@gmail.com>
2026-01-05 00:17:01 -0400

    Implement _calloc, malloc_checked, str_concat, string_nconcat, array_range functions, add _putchar implementations

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,15 +1,41 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @width: width of the grid
- * @height: height of the grid
- * Return: pointer to the grid, or NULL if it fails
+ * alloc_grid - creates a 2D array (grid) of integers initialized to 0
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: pointer to the 2D array, or NULL if it fails or if width/height <= 0
  */
 int **alloc_grid(int width, int height)
 {
-	(void)width;
-	(void)height;
-	return (NULL);
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
 }
